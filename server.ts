f829a7cf69cc9319355f8f1b444f18789806ba67
navalrawat <emailnaval@gmail.com>
2020-08-10 20:01:55 +0530

    spaceX to spacex

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ import { existsSync } from 'fs';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/spaceX/browser');
+  const distFolder = join(process.cwd(), 'dist/spacex/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
