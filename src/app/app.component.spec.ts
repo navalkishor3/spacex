8bd4439259b2b95890cee730a8f88d6af33ab032
navalrawat <emailnaval@gmail.com>
2020-08-10 20:02:58 +0530

    spaceX to spacex

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'spaceX'`, () => {
+  it(`should have as title 'spacex'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('spaceX');
+    expect(app.title).toEqual('spacex');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('spaceX app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('spacex app is running!');
   });
 });
