7f91d6f8d30c2a46e5ff1ff6db03fdd044f521f3
navalrawat <emailnaval@gmail.com>
2020-08-11 05:58:33 +0530

    seo & remove footer space

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   public launches;
+  public homeTitle = 'SpaceX Universal';
 
-  constructor() { }
+  constructor(private title: Title, private meta: Meta) { }
 
   ngOnInit(): void {
-
+  this.setSEO();
+  }
+  
+  setSEO(){
+    this.title.setTitle(this.homeTitle);
+    this.meta.addTags([
+      {name: 'keywords', content: 'This is spacex project with Server Side Render via Angular Universal'},
+      {name: 'description', content: 'This is spacex project with Server Side Render via Angular Universal'},
+      {name: 'robots', content: 'index, spacex'}
+    ]);
   }
 
   onLaunchesChange(data) {
