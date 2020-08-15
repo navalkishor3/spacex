07e7613d31d739c14e9cdd2bb4c9427843de6f85
navalrawat <emailnaval@gmail.com>
2020-08-16 01:55:58 +0530

    lint & test case for  travish do not deploy build

--- src/app/launch-filter/launch-filter.component.spec.ts
+++ src/app/launch-filter/launch-filter.component.spec.ts
@@ -1,46 +1,46 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { LaunchFilterComponent } from './launch-filter.component';
-// import { LaunchFilterService } from './launch-filter.service';
-// import { Router, ActivatedRoute } from '@angular/router';
-// import { Location } from '@angular/common';
-// import { RouterTestingModule } from '@angular/router/testing';
+import { LaunchFilterComponent } from './launch-filter.component';
+import { LaunchFilterService } from './launch-filter.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// // class FakeLaunchFilterService {
-// //   getLaunches : () => { }
-// // }
-// fdescribe('LaunchFilterComponent', () => {
-//   let component: LaunchFilterComponent;
-//   let fixture: ComponentFixture<LaunchFilterComponent>;
-//   const fakeLaunchFilterService = {
-//     getLaunches : () => { }
-//   };
-//   let FakeLocation ={};
+// class FakeLaunchFilterService {
+//   getLaunches : () => { }
+// }
+fdescribe('LaunchFilterComponent', () => {
+  let component: LaunchFilterComponent;
+  let fixture: ComponentFixture<LaunchFilterComponent>;
+  const fakeLaunchFilterService = {
+    getLaunches : () => { }
+  };
+  let FakeLocation ={};
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ LaunchFilterComponent ],
-//       providers:[
-//         {
-//         provide: LaunchFilterService,
-//         useValue : fakeLaunchFilterService
-//       }, Router, {
-//         provide: ActivatedRoute,
-//         useValue: {
-//           a :{}
-//         }
-//       }, Location]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LaunchFilterComponent ],
+      providers:[
+        {
+        provide: LaunchFilterService,
+        useValue : fakeLaunchFilterService
+      }, Router, {
+        provide: ActivatedRoute,
+        useValue: {
+          a :{}
+        }
+      }, Location]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(LaunchFilterComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LaunchFilterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
