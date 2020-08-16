730ed5467ab6bb78a994699db2cd96698f285650
navalrawat <emailnaval@gmail.com>
2020-08-16 05:37:46 +0530

    ng test

--- src/app/launch-filter/launch-filter.component.ts
+++ src/app/launch-filter/launch-filter.component.ts
@@ -33,7 +33,7 @@ export class LaunchFilterComponent implements OnInit {
 
   ngOnInit(): void {
     this.getLaunches({ all: true, launch_year_enable: false, launch_success_enable: false, land_success_enable: false });
-    console.log(8);
+    console.log(10);
   }
 
   getLaunches(filterObject) {
